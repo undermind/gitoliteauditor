f123efcf6a2c751e077299cacb3cd945c30a8d84
Mikhail Belyankin <mikhail.belyankin@exactprosystems.com>
2017-06-30 16:16:27 +0300

    Added code by gitaudit.php itself

--- gitaudit.php
+++ gitaudit.php
@@ -1,7 +1,23 @@
 #!/usr/bin/php
 <?php
+
+function Determinator($string)
+{
+  /*
+# Regex for repo name []repo foo/bar]
+REPO_NAME_REGEX = /\Arepo (.*)\n\z/
+# Regex for project description [foo/bar "owner" = "Description of repo"], owner may be empty
+REPO_INFO_REGEX = /\A([^ ]*) "([^"]*)" = "(.*)"\n\z/
+# Regex for access rights [  RW+ = username1 username2]
+REPO_ACCESS_REGEX = /\A  ([^ ]*) = (.*)\n\z/
+*/
+
+}
+
 //require_once 'vendor/autoload.php';
 $filename="../gitolite-admin/conf/gitolite.conf";
+
+$filename="gitolite.conf";
 if (file_exists($filename))
 {
 $groups=array();
@@ -9,10 +25,67 @@ $repos=array();
 $handle = fopen($filename, "r");
 if ($handle) {
     while (($line = fgets($handle)) !== false) {
+      $line = explode("#",$line);
+      if (isset($line[1])) 
+      {//echo "###".$line[1]; 
+      continue;}
+      $line=$line[0];
       $line = trim ($line);
-      echo $line."\n";
+
+      ////////////////////////////////////////////////////////////
+      if (preg_match("/repo (.*)/",$line, $regresult))
+      {
+        //new repo on hands
+        if (isset($currentrepo))
+        {
+          $repoinfo=array();
+          $repoinfo[]=$currentrepo; //reponame
+          $repoinfo[]=$reporights; //rights on repo
+          unset($currentrepo);unset($reporights);
+        }
+        $currentrepo=trim($regresult[1]);$reporights=array();
+        //echo "\nNew repo ".$currentrepo;
+        //print_r($regresult);
+        continue;        
+      }
+      ////////////////////////////////////////////////////////////
+      if (preg_match('/\@(.*) = (.*)\w*/',$line,$regresult))
+      {
+         //group
+         $groupsusers=explode(" ",$regresult[2]);
+          ///subgoups
+         $subcolletctor=array();
+         foreach($groupsusers as $subidx=>$subval)
+         {
+           if (substr($subval,0,1)==="@")
+           {
+             echo $subval;
+             $subcolletctor=array_merge($subcolletctor,$groups[substr($subval,1)]);
+             unset($groupsusers[$subidx]);
+           }
+         }
+         //merge subgroups
+         $groupsusers = array_merge($groupsusers,$subcolletctor);
+/// end of subgroups
+         $groupsusers=array_filter($groupsusers);//filter empty elements
+
+         $groups[$regresult[1]]= isset($groups[$regresult[1]])?array_merge($groups[$regresult[1]],$groupsusers):$groupsusers;
+
+         //print_r($regresult);
+         /*
+         echo "\n";
+         print_r($regresult[1]);
+         echo "=";
+         print_r($groups[$regresult[1]]);*/
+
+         continue;
+      }
+      //////////////////////////////////////////////////////////////
+/// looks like rule
+       
     }
     fclose($handle);
+    print_r($groups);
 } else {
 } }
 
