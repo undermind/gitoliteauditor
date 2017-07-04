8e146c0634e3d3abbe33c4a8c299c65f6b813f94
Mikhail Belyankin <mikhail.belyankin@exactprosystems.com>
2017-07-04 17:42:31 +0300

    Cleanup of access matrix from gitaudit

--- gitaudit.php
+++ gitaudit.php
@@ -10,7 +10,6 @@ if (file_exists($configfile))
 {
 $groups=array();$groups["all"]=array("!EvErYbOdY!");
 $repos=array();
- $repoheaders=array();
 $users=array();
 $handle = fopen($configfile, "r");
 if ($handle) {
@@ -62,7 +61,6 @@ if ($handle) {
           $ruleext=trim($regresult[2]);
           $ruletarget=trim($regresult[3]);
           //echo "Rule for repo ".$currentrepo." => ".$rule." (".$ruleext.") -> ".$ruletarget." \n";
-          $repoheaders=array_merge($repoheaders,array(empty($ruleext)?$currentrepo:$currentrepo."@".$ruleext));
           $ruletarget=ExplainList(trim($regresult[3]),true,$currentrepo);
           
           $ruletarget=array_filter($ruletarget);
@@ -112,66 +110,21 @@ echo (empty($ruleext)?$currentrepo:$currentrepo."@".$ruleext)."\n";
 // oneline for users WO keyss
 // foreach($users as $usrname=>$usr) if (!isset($usr["keys"])) {echo $usrname."\n";print_r($users[$usrname]);}
 
-ksort($users); ksort($repos);sort($repoheaders);$repoheaders=array_unique($repoheaders);
+ksort($users); ksort($repos);
    //print_r($groups);
    //
   // print_r($repos);
-  //  print_r($repoheaders);
   // print_r($users);
-
-   //print_r($repos["TestClone"]);
+   
+   print_r($repos["TestClone"]);
    //print_r($users["user.name"]);
    
-echo '<!DOCTYPE html><html><head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>GIToLITE MATRIX</title>';
-//echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>';
-echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js"></script>';
-echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/floatthead/2.0.3/jquery.floatThead.min.js" integrity="sha256-2Uhne1l42Oh0kPdYdDpIYx+mSMQO/HGOjZFEsBjQntY=" crossorigin="anonymous"></script>';
-//echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/floatthead/1.2.11/jquery.floatThead.js"></script>';
-echo '<style>
-            th
-            {
-                background-color: grey;
-                color: white;
-                text-align: center;
-                vertical-align: bottom;
-                height: 150px;
-                padding-bottom: 3px;
-                padding-left: 5px;
-                padding-right: 5px;
-            }
-
-            .verticalText
-            {
-                text-align: center;
-                vertical-align: middle;
-                width: 20px;
-                margin: 0px;
-                padding: 0px;
-                padding-left: 3px;
-                padding-right: 3px;
-                padding-top: 10px;
-                white-space: nowrap;
-                -webkit-transform: rotate(-90deg); 
-                -moz-transform: rotate(-90deg);                 
-            };
-        </style></head><body><center>';
-echo "<table border=1 class='table'>\n";
-echo "<thead><tr><th>user</th><th><div class=\"verticalText\">Keys</div></th>";foreach($repoheaders as $header) echo "<th><div class=\"verticalText\">".$header."</div></th>";echo "</tr></thead>\n";
-  foreach($users as $usrname=>$usr)
-  {
-    echo "<tr><td>".$usrname."</td>".(isset($usr["keys"])?"<td>".count($usr["keys"]):"<td bgcolor=\"gray\">")."</td>";
-    foreach($repoheaders as $header)
-    { 
-      echo (isset($usr[$header])?"<td>".(is_array($usr[$header])?implode("\n",$usr[$header]):$usr[$header]):"<td bgcolor=\"gray\">")."</td>";
-    }
-    echo "</tr>\n";
-  }
-  echo "</table><script>$('table').floatThead({ position: 'absolute'});</script></body></html>";
-
-   
+      
 } else { die("Config file read error1");
 } }
+
+
+
 function ExplainList($list, $doclean=false, $info=null)
 {
  $listusers=explode(" ",$list);
